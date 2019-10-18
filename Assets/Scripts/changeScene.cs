4ae44b02963ccc3a446d7353667e802c46b1fbf5
Sam <sam.carey312@gmail.com>
2019-10-17 21:07:17 -0500

    Added menus

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+
+    public void characterScene()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
+
+    public void titleScene()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+ 
+    public void protomapScene()
+    {
+        SceneManager.LoadScene("ProtoLevel");
+    }
+}
---
+++ Assets/Scripts/dogSelect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class dogSelect : MonoBehaviour
+{
+    public Light selectLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectLight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        selectLight.enabled = true;
+       
+    }
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("MapSelect");
+
+    }
+    void OnMouseExit()
+    {
+        selectLight.enabled = false;
+    }
+}
